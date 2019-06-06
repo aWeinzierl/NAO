d9711c6b8713469d104bfca5a758e7ce8486806f
Andreas Weinzierl <20560137+aWeinzierlo@users.noreply.github.com>
2019-06-06 17:26:05 +0200

    implement joint checking

--- src/main.cpp
+++ src/main.cpp
@@ -17,25 +17,38 @@
 #include <boost/date_time.hpp>
 #include <boost/thread/locks.hpp>
 #include <actionlib/client/simple_action_client.h>
+#include <unordered_set>
+
 using namespace std;
 
 typedef actionlib::SimpleActionClient<naoqi_bridge_msgs::JointAnglesWithSpeedAction> JointAnglesClient;
 
-bool stop_thread=false;
+bool stop_thread = false;
 
-void spinThread()
-{
+void spinThread() {
     ros::Rate r(30);
-    while(!stop_thread)
-    {
+    while (!stop_thread) {
         ros::spinOnce();
         r.sleep();
     }
 }
 
+struct Interval {
+    Interval(double lowerLimit, double upperLimit) : lowerLimit(lowerLimit), upperLimit(upperLimit) {}
 
-class Nao_control
-{
+
+    float lowerLimit;
+    float upperLimit;
+};
+
+struct ApproximatelyEqual {
+    template<typename KeyType>
+    bool operator()(const KeyType &lhs, const KeyType &rhs) const {
+        return lhs - rhs < std::numeric_limits<KeyType>::epsilon();
+    }
+};
+
+class Nao_control {
 public:
     // ros handler
     ros::NodeHandle nh_;
@@ -59,65 +72,73 @@ public:
 
     boost::thread *spin_thread;
 
-    Nao_control():jointAnglesClient("/joint_angles_action",true)
-    {
+    Nao_control() : jointAnglesClient("/joint_angles_action", true) {
         // subscribe to topic joint_states and specify that all data will be processed by function sensorCallback
-        sensor_data_sub=nh_.subscribe("/joint_states",1, &Nao_control::sensorCallback, this);
+        sensor_data_sub = nh_.subscribe("/joint_states", 1, &Nao_control::sensorCallback, this);
 
         // subscribe to topic bumper and specify that all data will be processed by function bumperCallback
-        bumper_sub=nh_.subscribe("/bumper",1, &Nao_control::bumperCallback, this);
+        bumper_sub = nh_.subscribe("/bumper", 1, &Nao_control::bumperCallback, this);
 
         // subscribe to topic tactile_touch and specify that all data will be processed by function tactileCallback
-        tactile_sub=nh_.subscribe("/tactile_touch",1, &Nao_control::tactileCallback, this);
+        tactile_sub = nh_.subscribe("/tactile_touch", 1, &Nao_control::tactileCallback, this);
 
-        stop_thread=false;
-        spin_thread=new boost::thread(&spinThread);
+        stop_thread = false;
+        spin_thread = new boost::thread(&spinThread);
     }
-    ~Nao_control()
-    {
-        stop_thread=true;
+
+    ~Nao_control() {
+        stop_thread = true;
         sleep(1);
         spin_thread->join();
     }
 
     // this callback function provides information about nao feet bumpers
-    void bumperCallback(const naoqi_bridge_msgs::Bumper::ConstPtr& bumperState)
-    {
+    void bumperCallback(const naoqi_bridge_msgs::Bumper::ConstPtr &bumperState) {
 
     }
 
     // this callback provides information about current head tactile buttons.
-    void tactileCallback(const naoqi_bridge_msgs::HandTouch::ConstPtr& tactileState)
-    {
+    void tactileCallback(const naoqi_bridge_msgs::HandTouch::ConstPtr &tactileState) {
 
 
     }
 
+    bool withinIntervalExclusive(const double value, const Interval &interval) {
+        return interval.lowerLimit < value && interval.upperLimit > value;
+    }
+
+    const std::unordered_set<
+            double,
+            std::unordered_set<double>::hasher,
+            ApproximatelyEqual,
+            std::unordered_set<double>::allocator_type> allowedStatesForHands = {0, 1};
+
     // this function checks joint limits of the left arm. You need to provide JointState vector
-    bool check_joint_limits_left_arm(sensor_msgs::JointState joints)
-    {
-        bool check=true;
+    bool check_joint_limits_left_arm(sensor_msgs::JointState joints) {
+        std::vector<double> &positions = joints.position;
 
-        /*
-         * TODO tutorial
-         */
-
-        return check;
+        return withinIntervalExclusive(positions.at(2), Interval(-2.0857, 2.0857))
+               && withinIntervalExclusive(positions.at(3), Interval(-0.3142, 1.3265))
+               && withinIntervalExclusive(positions.at(4), Interval(-2.0857, 2.0857))
+               && withinIntervalExclusive(positions.at(5), Interval(-1.5446, -0.0349))
+               && withinIntervalExclusive(positions.at(6), Interval(-1.8238, 1.8238))
+               && allowedStatesForHands.end() != allowedStatesForHands.find(positions.at(7));
     }
 
     // this function checks joint limits of the right arm. You need to provide JointState vector
-    bool check_joint_limits_right_arm(sensor_msgs::JointState joints)
-    {
-        bool check=true;
-        /*
-         * TODO tutorial
-         */
-        return check;
+    bool check_joint_limits_right_arm(sensor_msgs::JointState joints) {
+        std::vector<double> &positions = joints.position;
+
+        return withinIntervalExclusive(positions.at(20), Interval(-2.0857, 2.0857))
+               && withinIntervalExclusive(positions.at(21), Interval(-1.3265, 0.3142))
+               && withinIntervalExclusive(positions.at(22), Interval(-2.0857, 2.0857))
+               && withinIntervalExclusive(positions.at(23), Interval(0.0349, 1.5446))
+               && withinIntervalExclusive(positions.at(24), Interval(-1.8238, 1.8238))
+               && allowedStatesForHands.end() != allowedStatesForHands.find(positions.at(25));
     }
 
     // this callback recives info about current joint states
-    void sensorCallback(const sensor_msgs::JointState::ConstPtr& jointState)
-    {
+    void sensorCallback(const sensor_msgs::JointState::ConstPtr &jointState) {
         current_left_arm_state.name.clear();
         current_left_arm_state.position.clear();
         current_right_arm_state.name.clear();
@@ -125,7 +146,7 @@ public:
         current_head_legs_state.name.clear();
         current_head_legs_state.position.clear();
 
-        current_left_arm_state.header.stamp=ros::Time::now();
+        current_left_arm_state.header.stamp = ros::Time::now();
 
         current_left_arm_state.name.push_back(jointState->name.at(2));
         current_left_arm_state.position.push_back(jointState->position.at(2));
@@ -153,8 +174,7 @@ public:
         current_head_legs_state.position.push_back(jointState->position.at(0));
         current_head_legs_state.name.push_back(jointState->name.at(1));
         current_head_legs_state.position.push_back(jointState->position.at(1));
-        for(int i=7; i<20;i++)
-        {
+        for (int i = 7; i < 20; i++) {
             current_head_legs_state.name.push_back(jointState->name.at(i));
             current_head_legs_state.position.push_back(jointState->position.at(i));
         }
@@ -164,8 +184,7 @@ public:
 
 
     // this is main loop which should send commands to the nao arms.
-    void publish_joint_states()
-    {
+    void publish_joint_states() {
         /*
          * TODO tutorial
          */
@@ -173,26 +192,25 @@ public:
         //example of moving LShoulderPitch joint to -0.56 angle
         //Prepare the goal message
         naoqi_bridge_msgs::JointAnglesWithSpeedGoal action_execute;
-        action_execute.joint_angles.speed=0.05;
+        action_execute.joint_angles.speed = 0.05;
         //this variable controls if the angles are relative (1) or not relative (0).
-        action_execute.joint_angles.relative=0;
+        action_execute.joint_angles.relative = 0;
         action_execute.joint_angles.joint_names.push_back("LShoulderPitch");
         action_execute.joint_angles.joint_angles.push_back(-0.56);
         //send goal message to the robot using actionlib client
-        cout<<"adada"<<endl;
+        cout << "adada" << endl;
         jointAnglesClient.sendGoal(action_execute);
 
         //wait until the robot finish execution of current goal. The timeout is 2 seconds
         ros::Rate r_sleep(20);
-        while((!jointAnglesClient.waitForResult(ros::Duration(2.0)))&& (ros::ok()))
-        {
+        while ((!jointAnglesClient.waitForResult(ros::Duration(2.0))) && (ros::ok())) {
             r_sleep.sleep();
         }
     }
 
 };
-int main(int argc, char** argv)
-{
+
+int main(int argc, char **argv) {
     ros::init(argc, argv, "nao_tutorial6");
     Nao_control ic;
     sleep(2);
