1e93f3d72742f3da271c732f7046cdd108532e70
Andreas Weinzierl <20560137+aWeinzierl@users.noreply.github.com>
2019-06-08 00:49:05 +0200

    make private what should be private

--- src/NaoControl.h
+++ src/NaoControl.h
@@ -25,61 +25,14 @@ struct ApproximatelyEqual {
 
 class NaoControl {
 public:
-    // ros handler
-    ros::NodeHandle nh_;
-
-    // subscriber to joint states
-    ros::Subscriber sensor_data_sub;
-
-    // subscriber to bumpers states
-    ros::Subscriber bumper_sub;
-
-    // subscriber to head tactile states
-    ros::Subscriber tactile_sub;
-
-    // variables which store current states of the joints
-    sensor_msgs::JointState current_left_arm_state;
-    sensor_msgs::JointState current_right_arm_state;
-    sensor_msgs::JointState current_head_legs_state;
-
-    //define actionlib client for joint angles control
-    JointAnglesClient jointAnglesClient;
-
-    boost::thread *spin_thread;
 
     NaoControl();
 
     ~NaoControl();
 
-    // this callback function provides information about nao feet bumpers
-    void bumperCallback(const naoqi_bridge_msgs::Bumper::ConstPtr &bumperState);
-
-    // this callback provides information about current head tactile buttons.
-    void tactileCallback(const naoqi_bridge_msgs::HandTouch::ConstPtr &tactileState);
-
-    bool withinIntervalExclusive(double value, const Interval &interval);
-
-
-    // this function checks joint limits of the left arm. You need to provide JointState vector
-    bool check_joint_limits_left_arm(sensor_msgs::JointState joints);
-
-    // this function checks joint limits of the right arm. You need to provide JointState vector
-    bool check_joint_limits_right_arm(sensor_msgs::JointState joints);
-
-    // this callback recives info about current joint states
-    void sensorCallback(const sensor_msgs::JointState::ConstPtr &jointState);
-
-
     // this is main loop which should send commands to the nao arms.
     void publish_joint_states();
 
-    void ExecuteActionAsync(naoqi_bridge_msgs::JointAnglesWithSpeedGoal action);
-
-    void ExecuteAction(
-            naoqi_bridge_msgs::JointAnglesWithSpeedGoal action,
-            ros::Duration timeOut = ros::Duration(10));
-
-private:
     static constexpr Interval LEFT_SHOULDER_PITCH_LIMITS = Interval(-2.0857, 2.0857); // NOLINT(cert-err58-cpp)
     static constexpr Interval LEFT_SHOULDER_ROLL_LIMITS = Interval(-0.3142, 1.3265); // NOLINT(cert-err58-cpp)
     static constexpr Interval LEFT_ELBOW_YAW_LIMITS = Interval(-2.0857, 2.0857); // NOLINT(cert-err58-cpp)
@@ -102,4 +55,55 @@ private:
             std::unordered_set<double>::hasher,
             ApproximatelyEqual,
             std::unordered_set<double>::allocator_type> RIGHT_HAND_STATES = LEFT_HAND_STATES;
+
+
+private:
+
+    // ros handler
+    ros::NodeHandle nh_;
+
+    // subscriber to joint states
+    ros::Subscriber sensor_data_sub;
+
+    // subscriber to bumpers states
+    ros::Subscriber bumper_sub;
+
+    // subscriber to head tactile states
+    ros::Subscriber tactile_sub;
+
+    // variables which store current states of the joints
+    sensor_msgs::JointState current_left_arm_state;
+    sensor_msgs::JointState current_right_arm_state;
+    sensor_msgs::JointState current_head_legs_state;
+
+    //define actionlib client for joint angles control
+    JointAnglesClient jointAnglesClient;
+
+    boost::thread *spin_thread;
+
+
+    // this callback function provides information about nao feet bumpers
+    void bumperCallback(const naoqi_bridge_msgs::Bumper::ConstPtr &bumperState);
+
+    // this callback provides information about current head tactile buttons.
+    void tactileCallback(const naoqi_bridge_msgs::HandTouch::ConstPtr &tactileState);
+
+    bool withinIntervalExclusive(double value, const Interval &interval);
+
+
+    // this function checks joint limits of the left arm. You need to provide JointState vector
+    bool check_joint_limits_left_arm(sensor_msgs::JointState joints);
+
+    // this function checks joint limits of the right arm. You need to provide JointState vector
+    bool check_joint_limits_right_arm(sensor_msgs::JointState joints);
+
+    // this callback recives info about current joint states
+    void sensorCallback(const sensor_msgs::JointState::ConstPtr &jointState);
+
+    void ExecuteActionAsync(naoqi_bridge_msgs::JointAnglesWithSpeedGoal action);
+
+    void ExecuteAction(
+            naoqi_bridge_msgs::JointAnglesWithSpeedGoal action,
+            ros::Duration timeOut = ros::Duration(10));
+
 };
