f2926e76881d097f26e52c2c65e6b0335053a2e4
Andreas Weinzierl <20560137+aWeinzierl@users.noreply.github.com>
2019-06-09 17:52:23 +0200

    change head position on bumper press

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,15 @@ int main(int argc, char **argv) {
     sleep(2);
     ic.publish_joint_states();
 
+
+    double headPosition = 0;
+    ic.Bumper_sensor_state.subscribe([&ic, &headPosition] (const naoqi_bridge_msgs::Bumper& bumper) {
+        std::cout << "bumper: " << bumper.bumper;
+        std::cout << "state: " << bumper.state;
+
+        ic.Move_joint_to_position_async(NAO::ContinuousJoint::RIGHT_SHOULDER_PITCH, headPosition, 0.1);
+    });
+
     return 0;
 
 }
