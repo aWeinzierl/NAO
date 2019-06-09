606ec1446ecbbc60606ca2115c22320a13b73357
Andreas Weinzierl <20560137+aWeinzierl@users.noreply.github.com>
2019-06-09 19:58:33 +0200

    remove hands from continuous joint
    they are discrete

--- src/NAO/Joints.h
+++ src/NAO/Joints.h
@@ -11,14 +11,12 @@ namespace NAO {
         LEFT_ELBOW_YAW,
         LEFT_ELBOW_ROLL,
         LEFT_WRIST_YAW,
-        LEFT_HAND,
 
         RIGHT_SHOULDER_PITCH,
         RIGHT_SHOULDER_ROLL,
         RIGHT_ELBOW_YAW,
         RIGHT_ELBOW_ROLL,
         RIGHT_WRIST_YAW,
-        RIGHT_HAND,
     };
 
     enum class DiscreteJoint {
