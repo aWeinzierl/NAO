a247e549b138c32ff97cc1b1206b86973cd7e4aa
Andreas Weinzierl <20560137+aWeinzierl@users.noreply.github.com>
2019-06-10 21:42:55 +0200

    add namespace tags
    replace prefix

--- src/NAO/ContinuousJointSpecification.cpp
+++ src/NAO/ContinuousJointSpecification.cpp
@@ -1,21 +1,25 @@
 #include "ContinuousJointSpecification.h"
 
-const std::string &NAO::ContinuousJointSpecification::Get_name() const {
-    return m_name;
-}
+namespace NAO {
 
-const NAO::Interval &NAO::ContinuousJointSpecification::Get_value_range() const {
-    return m_value_range;
-}
+    const std::string &ContinuousJointSpecification::Get_name() const {
+        return m_name;
+    }
 
-NAO::ContinuousJointSpecification::ContinuousJointSpecification(std::string name, const NAO::Interval &valueRange,
-                                                                uint index)
-        : m_name(std::move(name)), m_value_range(valueRange), m_index(index) {}
+    const Interval &NAO::ContinuousJointSpecification::Get_value_range() const {
+        return m_value_range;
+    }
 
-bool NAO::ContinuousJointSpecification::Value_within_boundary(double value) const noexcept {
-    return m_value_range.lowerLimit < value && m_value_range.upperLimit > value;
-}
+    ContinuousJointSpecification::ContinuousJointSpecification(std::string name, const NAO::Interval &valueRange,
+                                                                    uint index)
+            : m_name(std::move(name)), m_value_range(valueRange), m_index(index) {}
 
-uint NAO::ContinuousJointSpecification::Get_index() const noexcept {
-    return m_index;
+    bool ContinuousJointSpecification::Value_within_boundary(double value) const noexcept {
+        return m_value_range.lowerLimit < value && m_value_range.upperLimit > value;
+    }
+
+    uint ContinuousJointSpecification::Get_index() const noexcept {
+        return m_index;
+    }
+
 }
